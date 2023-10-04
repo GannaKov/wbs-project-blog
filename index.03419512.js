2ec729f1083729972aefbb0c66b99f613073363d
GannaKov <104867435+GannaKov@users.noreply.github.com>
2023-10-04 22:51:01 +0000

    Deploying to gh-pages from @ GannaKov/wbs-project-blog@ed9a0d8ce37daca418cb736eca3cbd4e578c29d2 🚀

---
+++ index.03419512.js
@@ -0,0 +1,2 @@
+!function(){"use strict";document.addEventListener("DOMContentLoaded",(function(){var t=window.location.pathname.split("/").pop();document.querySelectorAll(".header-nav__link").forEach((function(n){n.getAttribute("href").includes(t)&&n.classList.add("current-link")}))}))}();
+//# sourceMappingURL=index.03419512.js.map
---
+++ index.c6b4070d.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){const e=window.location.pathname.split("/").pop();document.querySelectorAll(".header-nav__link").forEach((t=>{t.getAttribute("href").includes(e)&&t.classList.add("current-link")}))}));
+//# sourceMappingURL=index.c6b4070d.js.map
