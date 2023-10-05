aced85c169a0e82717042e7d46985490b932f18b
GannaKov <zlatta2000@gmail.com>
2023-10-05 17:00:30 +0200

    scss vs css?, click on nav link

--- src/index.js
+++ src/index.js
@@ -4,8 +4,19 @@ document.addEventListener('DOMContentLoaded', function () {
   const links = document.querySelectorAll('.header-nav__link');
 
   links.forEach(link => {
-    if (link.getAttribute('href').includes(currentPage)) {
+    if (link.getAttribute('href').split('/').pop() === currentPage) {
       link.classList.add('current-link');
     }
   });
+
+  links.forEach(link => {
+    link.addEventListener('click', function (event) {
+      if (link.getAttribute('href').split('/').pop() === currentPage) {
+        link.classList.add('current-link');
+        event.preventDefault();
+      }
+    });
+  });
+
+  // event.preventDefault();
 });
