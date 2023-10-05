f52081abf659bb8111cf95955768e02df9ef4820
GannaKov <104867435+GannaKov@users.noreply.github.com>
2023-10-05 15:01:04 +0000

    Deploying to gh-pages from @ GannaKov/wbs-project-blog@aced85c169a0e82717042e7d46985490b932f18b 🚀

---
+++ about.ad9cb5bf.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){const t=window.location.pathname.split("/").pop(),e=document.querySelectorAll(".header-nav__link");e.forEach((e=>{e.getAttribute("href").split("/").pop()===t&&e.classList.add("current-link")})),e.forEach((e=>{e.addEventListener("click",(function(n){e.getAttribute("href").split("/").pop()===t&&(e.classList.add("current-link"),n.preventDefault())}))}))}));
+//# sourceMappingURL=about.ad9cb5bf.js.map
