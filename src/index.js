c16a98d3cd9b6fa55f6942a63d46d2414496aa72
GannaKov <zlatta2000@gmail.com>
2023-10-09 04:20:29 +0200

    dropdown

--- src/index.js
+++ src/index.js
@@ -27,4 +27,52 @@ document.addEventListener('DOMContentLoaded', function () {
   // event.preventDefault();
 });
 //-------------------------
-const dropdownCountry = document.querySelector('.');
+$('#country').selectmenu().selectmenu('menuWidget').addClass('overflow');
+$('.dpd-select').selectmenu({
+  width: 250,
+});
+//--------------------
+//-----------------
+// console.log($('.dpd-select').selectmenu('option'));
+
+// const options = $('.dpd-select').selectmenu('option');
+// console.log(options);
+// $('.dpd-select').selectmenu({
+//   change: function (event, ui) {
+//     console.log(event), console.log(event.currentTarget.textContent);
+//   },
+// });
+// $('.dpd-select').selectmenu({
+//   select: function (event, ui) {
+//     console.log('2', event);
+//   },
+// });
+//---------------
+const dpd = document.querySelector('.dpd-select');
+const items = [
+  'Italy',
+  'Germany',
+  'Egypt',
+  'Greece',
+  'Sweden',
+  'Poland',
+  'Slovakia',
+  'Finland',
+  'Thailand',
+  'Philippines',
+  'Cuba',
+  'Mexico',
+  'Sweden',
+  'Austria',
+];
+
+function renderMenu(el, arr) {
+  const murkup = arr
+    .map(item => `<option value=${item}>${item} </option>`)
+    .join('');
+  el.insertAdjacentHTML('beforeend', murkup);
+}
+renderMenu(dpd, items);
+//---
+const dpdButton = document.querySelector('.ui-selectmenu-text');
+dpdButton.textContent = 'Choose the country';
