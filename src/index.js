197ae29d37e7b992f985102eb190177c55e85689
GannaKov <zlatta2000@gmail.com>
2023-10-11 16:14:35 +0200

    before after

--- src/index.js
+++ src/index.js
@@ -1,52 +1,77 @@
 'use strict';
-if (
-  window.location.pathname === '/' ||
-  window.location.pathname === '/wbs-project-blog/'
-) {
-  window.location.href = '/wbs-project-blog/index.html';
-}
-document.addEventListener('DOMContentLoaded', function () {
+//-----------------
+const basePath = '/wbs-project-blog/';
+function setCurrentLink() {
   const currentPage = window.location.pathname.split('/').pop();
   const links = document.querySelectorAll('.header-nav__link');
 
   links.forEach(link => {
     if (link.getAttribute('href').split('/').pop() === currentPage) {
       link.classList.add('current-link');
+    } else {
+      link.classList.remove('current-link');
     }
   });
+}
+document.addEventListener('DOMContentLoaded', function () {
+  setCurrentLink();
 
+  const links = document.querySelectorAll('.header-nav__link');
   links.forEach(link => {
     link.addEventListener('click', function (event) {
-      if (link.getAttribute('href').split('/').pop() === currentPage) {
-        link.classList.add('current-link');
+      const href = link.getAttribute('href');
+      const lastPathSegment = href.split('/').pop();
+
+      if (lastPathSegment === window.location.pathname.split('/').pop()) {
         event.preventDefault();
       }
     });
   });
-
-  // event.preventDefault();
 });
+
+// Перенаправляем на базовую страницу, если путь не указан
+if (
+  window.location.pathname === '/' ||
+  window.location.pathname === `${basePath}`
+) {
+  window.location.href = `${basePath}index.html`;
+}
+//-------------------
+// if (
+//   window.location.pathname === '/' ||
+//   window.location.pathname === '/wbs-project-blog/'
+// ) {
+//   window.location.href = '/wbs-project-blog/index.html';
+// }
+// document.addEventListener('DOMContentLoaded', function () {
+//   const currentPage = window.location.pathname.split('/').pop();
+//   const links = document.querySelectorAll('.header-nav__link');
+
+//   links.forEach(link => {
+//     if (link.getAttribute('href').split('/').pop() === currentPage) {
+//       link.classList.add('current-link');
+//     }
+//   });
+
+//   links.forEach(link => {
+//     link.addEventListener('click', function (event) {
+//       if (link.getAttribute('href').split('/').pop() === currentPage) {
+//         link.classList.add('current-link');
+//         event.preventDefault();
+//       }
+//     });
+//   });
+
+//
+// });
 //-------------------------
-$('#country').selectmenu().selectmenu('menuWidget').addClass('overflow');
-$('.dpd-select').selectmenu({
-  width: 240,
-});
+console.log('aaa', href.split('/').pop());
+// '#country'.selectmenu().selectmenu('menuWidget').addClass('overflow');
+// '.dpd-select'.selectmenu({
+//   width: 240,
+// });
 //--------------------
-//-----------------
-// console.log($('.dpd-select').selectmenu('option'));
 
-// const options = $('.dpd-select').selectmenu('option');
-// console.log(options);
-// $('.dpd-select').selectmenu({
-//   change: function (event, ui) {
-//     console.log(event), console.log(event.currentTarget.textContent);
-//   },
-// });
-// $('.dpd-select').selectmenu({
-//   select: function (event, ui) {
-//     console.log('2', event);
-//   },
-// });
 //---------------
 const dpd = document.querySelector('.dpd-select');
 const countries = [
