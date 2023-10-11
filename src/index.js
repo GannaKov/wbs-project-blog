60d02368767d751384609a95ba8dab0ee9bae609
GannaKov <zlatta2000@gmail.com>
2023-10-11 16:39:34 +0200

    fix error with dropdown

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ if (
 //
 // });
 //-------------------------
-console.log('aaa', href.split('/').pop());
+
 // '#country'.selectmenu().selectmenu('menuWidget').addClass('overflow');
 // '.dpd-select'.selectmenu({
 //   width: 240,
@@ -73,31 +73,3 @@ console.log('aaa', href.split('/').pop());
 //--------------------
 
 //---------------
-const dpd = document.querySelector('.dpd-select');
-const countries = [
-  'Italy',
-  'Germany',
-  'Egypt',
-  'Greece',
-  'Sweden',
-  'Poland',
-  'Slovakia',
-  'Finland',
-  'Thailand',
-  'Philippines',
-  'Cuba',
-  'Mexico',
-  'Sweden',
-  'Austria',
-];
-const sortedCountries = [...countries].sort((a, b) => a.localeCompare(b));
-function renderMenu(el, arr) {
-  const murkup = arr
-    .map(item => `<option value=${item}>${item} </option>`)
-    .join('');
-  el.insertAdjacentHTML('beforeend', murkup);
-}
-renderMenu(dpd, sortedCountries);
-//---
-const dpdButton = document.querySelector('.ui-selectmenu-text');
-dpdButton.textContent = 'Choose the country';
---
+++ src/js/customFunction/dropdown.js
@@ -0,0 +1,35 @@
+$('#country').selectmenu().selectmenu('menuWidget').addClass('overflow');
+$('.dpd-select').selectmenu({
+  width: 240,
+});
+
+const dpd = document.querySelector('.dpd-select');
+const countries = [
+  'Italy',
+  'Germany',
+  'Egypt',
+  'Greece',
+  'Sweden',
+  'Poland',
+  'Slovakia',
+  'Finland',
+  'Thailand',
+  'Philippines',
+  'Cuba',
+  'Mexico',
+  'Sweden',
+  'Austria',
+];
+
+const sortedCountries = [...countries].sort((a, b) => a.localeCompare(b));
+function renderMenu(el, arr) {
+  const murkup = arr
+    .map(item => `<option value=${item}>${item} </option>`)
+    .join('');
+  el.insertAdjacentHTML('beforeend', murkup);
+}
+
+renderMenu(dpd, sortedCountries);
+//---
+const dpdButton = document.querySelector('.ui-selectmenu-text');
+dpdButton.textContent = 'Choose the country';
