f07ad6d5dbd2d7738118779ed120707e6113ef5a
GannaKov <zlatta2000@gmail.com>
2023-10-16 20:53:32 +0200

    js load one article

--- src/article-page.js
+++ src/article-page.js
@@ -1,1 +1,57 @@
-//'use strict';
+'use strict';
+import axios from 'axios';
+const BASEURL = 'https://posts.free.beeceptor.com/posts';
+const oneArticle = document.querySelector('.oneArticle-article');
+const oneArticleComments = document.querySelector('.oneArticle-comments');
+
+function getParameterByName(name, url) {
+  //   console.log(name, url);
+  if (!url) url = window.location.href;
+  //   console.log(url);
+  //   name = name.replace(/[[\]]/g, '\\$&');
+  //   console.log(name);
+  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    results = regex.exec(url);
+  //   console.log(results);
+  if (!results) return null;
+  if (!results[2]) return '';
+  //   console.log(results[2].replace(/\+/g, ' '));
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const postId = getParameterByName('post');
+  if (postId) {
+    fetchArticle(postId);
+  }
+});
+
+async function fetchData(postId) {
+  const response = await axios(`${BASEURL}/${postId}`);
+
+  return response;
+}
+function fetchArticle(postId) {
+  fetchData(postId).then(response => {
+    articleMarkup(response.data);
+  });
+}
+function articleMarkup(post) {
+  const basicMarkup = `<h1 class="article__title">${post.title}</h1>
+  <p class="article__date">${post.date}</p>
+  <div class="article__internal">
+   <img class="oneArticle__img" src=${post.url} alt="" />
+        <div class="article__text">
+        <p>${post.article}
+         </p>
+        </div>
+      </div>`;
+
+  oneArticle.innerHTML = basicMarkup;
+  if (post.comments) {
+    const commentsQuantityDiv = `<div class="comments-quantity-style"><p class="text-dashed "
+          ><span class="comments-quantity">${post.comments.length}</span>&nbsp;comments:</p></div>`;
+    oneArticleComments.innerHTML = commentsQuantityDiv;
+  }
+}
+//---------------------------------------
--- src/js/controls/readMore.js
+++ src/js/controls/readMore.js
@@ -1,20 +1,47 @@
-import axios from 'axios';
-const BASEURL = 'https://posts.free.beeceptor.com/posts';
+// import axios from 'axios';
+// const BASEURL = 'https://posts.free.beeceptor.com/posts';
 
 const readMore = document.querySelector('.content-list__link-1');
-console.log(readMore);
+// const oneArticle = document.querySelector('.oneArticle-article');
+
 export function onListenerReadMore() {
   readMore.addEventListener('click', onReadMoreClick);
 }
 
-export function onReadMoreClick(event) {
-  //event.preventDefault();
+function onReadMoreClick(event) {
+  event.preventDefault();
   const postId = 1;
-  fetchArticle(postId);
+
+  window.location.href = `./article-page.html?post=${postId}`;
   readMore.removeEventListener('click', onReadMoreClick);
 }
-async function fetchArticle(postId) {
-  const response = await axios(`${BASEURL}/${postId}`);
-  console.log(response);
-  return response;
-}
+// function onReadMoreClick(event) {
+//   event.preventDefault();
+//   const postId = 1;
+//   fetchArticle(postId);
+//   readMore.removeEventListener('click', onReadMoreClick);
+//}
+// async function fetchData(postId) {
+//   const response = await axios(`${BASEURL}/${postId}`);
+//   console.log(response.data);
+//   return response;
+// }
+// function fetchArticle(postId) {
+//   fetchData(postId).then(response => {
+//     articleMarkup(response.data);
+//   });
+// }
+// function articleMarkup(post) {
+//   console.log('in articleMarkup ');
+//   const basicMarkup = `<h1 class="article__title">${post.title}</h1>
+//   <p class="article__date">${post.date}</p>
+//   <div class="article__internal">
+//    <img class="oneArticle__img" src=${post.url} alt="" />
+//         <div class="article__text">
+//         <p>${post.article}
+//          </p>
+//         </div>
+//       </div>`;
+//   console.log('basic', basicMarkup);
+//   oneArticle.innerHTML = basicMarkup;
+// }
